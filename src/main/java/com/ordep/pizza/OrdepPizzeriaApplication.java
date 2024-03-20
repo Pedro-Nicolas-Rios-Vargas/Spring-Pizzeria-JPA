092c7b7c029e808ed5f380ee0ae72d90aa5f687f
Pedro Nicolás Rios Vargas <pv123124@gmail.com>
2024-03-20 13:24:08 -0600

    Project base structure

---
+++ src/main/java/com/ordep/pizza/OrdepPizzeriaApplication.java
@@ -0,0 +1,13 @@
+package com.ordep.pizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdepPizzeriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdepPizzeriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ordep/pizza/OrdepPizzeriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ordep.pizza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdepPizzeriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
