b3532e2aa3e290bc3b6f397e85376c196aee5f9d
Pedro Nicolás Rios Vargas <pv123124@gmail.com>
2024-03-25 15:51:27 -0600

    JPA Query Methods: And, Or, True, IgnoreCase

--- src/main/java/com/ordep/pizza/persistence/repository/PizzaRepository.java
+++ src/main/java/com/ordep/pizza/persistence/repository/PizzaRepository.java
@@ -3,6 +3,11 @@ package com.ordep.pizza.persistence.repository;
 import com.ordep.pizza.persistence.entity.PizzaEntity;
 import org.springframework.data.repository.ListCrudRepository;
 
+import java.util.List;
+
 public interface PizzaRepository
         extends ListCrudRepository<PizzaEntity, Integer> {
+    List<PizzaEntity> findAllByAvailableTrueOrderByPrice();
+
+    PizzaEntity findByAvailableTrueAndNameIgnoreCase(String name);
 }
