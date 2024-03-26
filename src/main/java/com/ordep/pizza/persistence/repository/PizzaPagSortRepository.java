48160c05a15b1e1c22afaeb84fe083715a0754b6
Pedro Nicolás Rios Vargas <pv123124@gmail.com>
2024-03-25 23:24:05 -0600

    Added Sort and Pagination over PizzaEntity

--- src/main/java/com/ordep/pizza/persistence/repository/PizzaPagSortRepository.java
+++ src/main/java/com/ordep/pizza/persistence/repository/PizzaPagSortRepository.java
@@ -1,7 +1,10 @@
 package com.ordep.pizza.persistence.repository;
 
 import com.ordep.pizza.persistence.entity.PizzaEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.ListPagingAndSortingRepository;
 
 public interface PizzaPagSortRepository extends ListPagingAndSortingRepository<PizzaEntity, Integer> {
+    Page<PizzaEntity> findByAvailableTrue(Pageable pageable);
 }
