aafa5b47ec56bc8368b7af849677d1a989f9b0ae
Pedro Nicolás Rios Vargas <pv123124@gmail.com>
2024-04-02 23:04:28 -0600

    SecurityConfig change. Only authenticated requests are admited.

--- src/main/java/com/ordep/pizza/web/config/SecurityConfig.java
+++ src/main/java/com/ordep/pizza/web/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.ordep.pizza.web.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -16,8 +17,9 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authorize -> {
                     authorize
                             .anyRequest()
-                            .permitAll();
-                });
+                            .authenticated();
+                })
+                .httpBasic(Customizer.withDefaults());
         return http.build();
     }
 }
