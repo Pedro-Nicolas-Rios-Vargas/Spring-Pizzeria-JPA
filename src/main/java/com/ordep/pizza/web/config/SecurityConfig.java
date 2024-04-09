36a411d1392638bf4129f19c03e060470f5b7e64
Pedro Nicolás Rios Vargas <pv123124@gmail.com>
2024-04-09 13:58:40 -0600

    Disabled CSRF Filter and added specification to defaults CORS

--- src/main/java/com/ordep/pizza/web/config/SecurityConfig.java
+++ src/main/java/com/ordep/pizza/web/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,6 +15,8 @@ public class SecurityConfig {
             throws Exception {
 
         http
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(authorize -> {
                     authorize
                             .anyRequest()
