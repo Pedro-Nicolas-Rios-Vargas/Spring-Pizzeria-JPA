6f301c28f3fda8573f939bdcccab360514d42bf7
Pedro Nicolás Rios Vargas <pv123124@gmail.com>
2024-04-18 23:45:47 -0600

    JWT's validation methods created.

--- src/main/java/com/ordep/pizza/web/config/JwtUtil.java
+++ src/main/java/com/ordep/pizza/web/config/JwtUtil.java
@@ -2,6 +2,8 @@ package com.ordep.pizza.web.config;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
@@ -20,5 +22,23 @@ public class JwtUtil {
                 .withIssuedAt(new Date())
                 .withExpiresAt(new Date(System.currentTimeMillis() + TimeUnit.DAYS.toMillis(15)))
                 .sign(ALGORITHM);
+    }
+
+    public boolean isValid(String jwt) {
+        try {
+            JWT.require(ALGORITHM)
+                    .build()
+                    .verify(jwt);
+            return true;
+        } catch (JWTVerificationException e) {
+            return false;
+        }
+    }
+
+    public String getUsername(String jwt) {
+        return JWT.require(ALGORITHM)
+                .build()
+                .verify(jwt)
+                .getSubject();
     }
 }
